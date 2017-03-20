c8bfdc4f55fc0b8cda4fdebf0f7dac42aecd1f58
smsadmin-001 <qiong.wu@iplustek.com>
2017-03-20 10:33:30 +0800

    print hello worl

---
+++ src/com/wu/Main.java
@@ -0,0 +1,12 @@
+package com.wu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my git is run");
+		System.out.println("hello wold");
+
+	}
+
+}
